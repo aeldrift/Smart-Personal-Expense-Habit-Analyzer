6e8f6ba51150a3106fbe7ec570f980930b356656
iteesha22 <jindalteesha22@gmail.com>
2025-12-25 00:45:27 +0530

    committed

--- expense_tracker/expense.py
+++ expense_tracker/expense.py
@@ -4,3 +4,14 @@ class Expense:
         self.amount = amount
         self.category = category
         self.note = note
+
+expenses = []
+# while True: or
+
+n = int(input("How many expenses do you want to add?"))
+
+for i in range(n):
+    print(f"\nExpense {i+1}")
+    amount = int(input("Enter amount: "))
+    category = input("Enter category: ")
+    note = input("Enter note: ")
